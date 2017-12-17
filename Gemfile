8e8d445c25ba4bf7c3234a884e3f482ec325104a
Ben Sarrazin <ben@7swift.io>
2017-12-17 16:05:01 -0500

    Having fun

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem 'bundler'
+gem 'cocoapods-playgrounds'
+gem 'cocoapods'
+gem 'colorize'
+gem 'fastlane'
+gem 'pry'
+gem 'pry-byebug'
---
+++ HavingFun/HavingFun.h
@@ -0,0 +1,19 @@
+//
+//  HavingFun.h
+//  HavingFun
+//
+//  Created by Ben Sarrazin on 2017-12-17.
+//  Copyright © 2017 7swift IO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HavingFun.
+FOUNDATION_EXPORT double HavingFunVersionNumber;
+
+//! Project version string for HavingFun.
+FOUNDATION_EXPORT const unsigned char HavingFunVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HavingFun/PublicHeader.h>
+
+
