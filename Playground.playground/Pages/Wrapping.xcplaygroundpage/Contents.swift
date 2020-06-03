fda69074e5e129d0a0e07dd101f8bfefa75a7af2
Ben Sarrazin <b@srz.io>
2020-06-03 13:43:24 -0400

    Better wrapping

--- Playground.playground/Pages/Wrapping.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Wrapping.xcplaygroundpage/Contents.swift
@@ -2,30 +2,78 @@ import Foundation
 
 // Composing through wrapping
 
-protocol DataStore {
-    func upsert(completion: () -> Void)
+protocol DynamicFeatureStore {
+    func value(for key: String, completion: @escaping (String) -> Void)
 }
 
-class InMemoryDataStore: DataStore {
-    func upsert(completion: () -> Void) {
-        completion()
+class SplitDynamicFeatureStore: DynamicFeatureStore {
+    func value(for key: String, completion: @escaping (String) -> Void) {
+        DispatchQueue.main.async {
+            completion(key)
+        }
     }
 }
 
-class SynchronousDataStore: DataStore {
-    let inner: DataStore
+class SynchronousDynamicFeatureStore<T: DynamicFeatureStore> {
 
-    init(inner: DataStore) {
+    private let inner: T
+
+    init(inner: T) {
         self.inner = inner
     }
-    func upsert(completion: () -> Void) {
+
+    func value(for key: String) -> String {
+
         let group = DispatchGroup()
         group.enter()
 
-        inner.upsert {
-            group.leave()
+        var result: String!
+        inner.value(for: key) { value in
+            result = value
+            group.leaver()
         }
 
         group.wait()
+
+        return result
     }
+
 }
+
+class CachingSynchronousDynamicFeatureStore {
+
+    private let inner: SynchronousDynamicFeatureStore
+
+    private var cache: [String: String] = [:]
+
+    init(inner: SynchronousDynamicFeatureStore) {
+        self.inner = inner
+    }
+
+    func value(for key: String, strategy: Strategy) -> String {
+        guard cache[key] == nil else {
+            return cache[key]
+        }
+
+        let result = inner.value(for: key)
+        cache[key] = result
+
+        return result
+    }
+
+}
+
+let store: DynamicFeatureStore = SynchronousDynamicFeatureStore(inner: SplitDynamicFeatureStore())
+
+let result1 = store.value(for: "key", strategy: .cacheOnlyAndFetch)
+
+protocol SomeSideEffectDFS {
+
+}
+
+class SplitSSEDFS: SomeSideEffectDFS {
+    inner
+}
+
+
+let egg = ThreePoundsEgg(RedEgg(Egg()))
